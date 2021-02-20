b9d5c60e3ebc0f81a8c815ac36473d68b0211c53
adlong <3140618@163.com>
2021-02-20 16:50:16 +0800

    add rest market api

--- alpha/platforms/huobi_usdt_swap/rest_market.py
+++ alpha/platforms/huobi_usdt_swap/rest_market.py
@@ -1,11 +1,132 @@
 from alpha.platforms.huobi_usdt_swap.http_utils import *
 
+
 class RestMarket:
     def __init__(self, host=None):
         if host is None:
             host = "api.btcgateway.pro"
         self.host = host
 
-    def get_contract_info(self, params:dict=None)->json:
+    def get_contract_info(self, params: dict = None) -> json:
         path = "/linear-swap-api/v1/swap_contract_info"
+        return get(self.host, path, params)
+
+    def get_index_info(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_index"
+        return get(self.host, path, params)
+
+    def get_price_limit(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_price_limit"
+        return get(self.host, path, params)
+
+    def get_open_interest(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_open_interest"
+        return get(self.host, path, params)
+
+    def get_depth(self, params: dict = None) -> json:
+        path = "/linear-swap-ex/market/depth"
+        return get(self.host, path, params)
+
+    def get_kline(self, params: dict = None) -> json:
+        path = "/linear-swap-ex/market/history/kline"
+        return get(self.host, path, params)
+
+    def get_mark_price_kline(self, params: dict = None) -> json:
+        path = "/index/market/history/linear_swap_mark_price_kline"
+        return get(self.host, path, params)
+
+    def get_merged(self, params: dict = None) -> json:
+        path = "/linear-swap-ex/market/detail/merged"
+        return get(self.host, path, params)
+
+    def get_batch_merged(self, params: dict = None) -> json:
+        path = "/linear-swap-ex/market/detail/batch_merged"
+        return get(self.host, path, params)
+
+    def get_trade(self, params: dict = None) -> json:
+        path = "/linear-swap-ex/market/trade"
+        return get(self.host, path, params)
+
+    def get_history_trade(self, params: dict = None) -> json:
+        path = "/linear-swap-ex/market/history/trade"
+        return get(self.host, path, params)
+
+    def get_risk_info(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_risk_info"
+        return get(self.host, path, params)
+
+    def get_insurance_fund(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_insurance_fund"
+        return get(self.host, path, params)
+
+    def isolated_get_adjustfactor(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_adjustfactor"
+        return get(self.host, path, params)
+
+    def cross_get_adjustfactor(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_cross_adjustfactor"
+        return get(self.host, path, params)
+    
+    def get_estimated_settlement_price(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_estimated_settlement_price"
+        return get(self.host, path, params)
+
+    def get_open_interest(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_his_open_interest"
+        return get(self.host, path, params)
+
+    def isolated_get_ladder_margin(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_ladder_margin"
+        return get(self.host, path, params)
+
+    def cross_get_ladder_margin(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_cross_ladder_margin"
+        return get(self.host, path, params)
+
+    def get_elite_account_ratio(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_elite_account_ratio"
+        return get(self.host, path, params)
+
+    def get_elite_position_ratio(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_elite_position_ratio"
+        return get(self.host, path, params)
+
+    def isolated_get_api_state(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_api_state"
+        return get(self.host, path, params)
+
+    def cross_get_transfer_state(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_cross_transfer_state"
+        return get(self.host, path, params)
+
+    def cross_get_trade_state(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_cross_trade_state"
+        return get(self.host, path, params)
+
+    def get_funding_rate(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_funding_rate"
+        return get(self.host, path, params)
+
+    def get_historical_funding_rate(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_historical_funding_rate"
+        return get(self.host, path, params)
+
+    def get_liquidation_orders(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_liquidation_orders"
+        return get(self.host, path, params)
+
+    def get_settlement_records(self, params: dict = None) -> json:
+        path = "/linear-swap-api/v1/swap_settlement_records"
+        return get(self.host, path, params)
+
+    def get_premium_index_kline(self, params: dict = None) -> json:
+        path = "/index/market/history/linear_swap_premium_index_kline"
+        return get(self.host, path, params)
+
+    def get_estimated_rate_kline(self, params: dict = None) -> json:
+        path = "/index/market/history/linear_swap_estimated_rate_kline"
+        return get(self.host, path, params)
+
+    def get_basis(self, params: dict = None) -> json:
+        path = "/index/market/history/linear_swap_basis"
         return get(self.host, path, params)
--- tests/test_rest_market.py
+++ tests/test_rest_market.py
@@ -13,10 +13,193 @@ logger.addHandler(stream_handler)
 
 
 class TestRestMarket(unittest.TestCase):
+
+    def setUp(self):
+        self.api = RestMarket()
+
     def test_get_contract_info(self):
-        api = RestMarket()
-        result = api.get_contract_info(
+        result = self.api.get_contract_info(
             {"contract_code": "btc-usdt", "support_margin_mode": "all"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_index_info(self):
+        result = self.api.get_index_info(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_price_limit(self):
+        result = self.api.get_price_limit(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_open_interest(self):
+        result = self.api.get_open_interest(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_depth(self):
+        result = self.api.get_depth(
+            {"contract_code": "btc-usdt", "type": "step0"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_kline(self):
+        result = self.api.get_kline(
+            {"contract_code": "btc-usdt", "period": "1min", "size": 10})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_mark_price_kline(self):
+        result = self.api.get_mark_price_kline(
+            {"contract_code": "btc-usdt", "period": "1min", "size": 10})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_merged(self):
+        result = self.api.get_merged(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_batch_merged(self):
+        result = self.api.get_batch_merged(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_trade(self):
+        result = self.api.get_trade(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_history_trade(self):
+        result = self.api.get_history_trade(
+            {"contract_code": "btc-usdt", "size": 2000})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_risk_info(self):
+        result = self.api.get_risk_info(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_insurance_fund(self):
+        result = self.api.get_insurance_fund(
+            {"contract_code": "btc-usdt", "page_index": 2, "page_size": 100})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_isolated_get_adjustfactor(self):
+        result = self.api.isolated_get_adjustfactor(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_cross_get_adjustfactor(self):
+        result = self.api.cross_get_adjustfactor(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_estimated_settlement_price(self):
+        result = self.api.get_estimated_settlement_price(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_open_interest(self):
+        result = self.api.get_open_interest(
+            {"contract_code": "btc-usdt", "period": "60min", "size": 100, "amount_type": 1})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_isolated_get_ladder_margin(self):
+        result = self.api.isolated_get_ladder_margin(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_cross_get_ladder_margin(self):
+        result = self.api.cross_get_ladder_margin(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_elite_account_ratio(self):
+        result = self.api.get_elite_account_ratio(
+            {"contract_code": "btc-usdt", "period": "5min"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_elite_position_ratio(self):
+        result = self.api.get_elite_position_ratio(
+            {"contract_code": "btc-usdt", "period": "5min"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_isolated_get_api_state(self):
+        result = self.api.isolated_get_api_state(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_cross_get_transfer_state(self):
+        result = self.api.cross_get_transfer_state(
+            {"margin_account": "usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_cross_get_trade_state(self):
+        result = self.api.cross_get_trade_state(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_funding_rate(self):
+        result = self.api.get_funding_rate(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_historical_funding_rate(self):
+        result = self.api.get_historical_funding_rate(
+            {"contract_code": "btc-usdt", "page_index": 2, "page_size": 50})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_liquidation_orders(self):
+        result = self.api.get_liquidation_orders(
+            {"contract_code": "btc-usdt", "trade_type": 0, "create_date": 90, "page_index": 2, "page_size": 50})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_settlement_records(self):
+        result = self.api.get_settlement_records(
+            {"contract_code": "btc-usdt"})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_premium_index_kline(self):
+        result = self.api.get_premium_index_kline(
+            {"contract_code": "btc-usdt", "period": "1min", "size": 100})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_estimated_rate_kline(self):
+        result = self.api.get_estimated_rate_kline(
+            {"contract_code": "btc-usdt", "period": "1min", "size": 100})
+        logger.info(result)
+        self.assertEqual('ok', result['status'])
+
+    def test_get_basis(self):
+        result = self.api.get_basis(
+            {"contract_code": "btc-usdt", "period": "1min", "basis_price_type": "close", "size": 100})
         logger.info(result)
         self.assertEqual('ok', result['status'])
 
