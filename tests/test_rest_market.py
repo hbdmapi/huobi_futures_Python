c1f63bbaa0ed91ec73ed5d7e5eb860119a0eb175
adlong <3140618@163.com>
2021-02-20 14:52:06 +0800

    add test config in vscode

--- tests/test_rest_market.py
+++ tests/test_rest_market.py
@@ -1,22 +1,25 @@
+import logging
+from alpha.platforms.huobi_usdt_swap.rest_market import *
 import sys
 import unittest
 from config import *
 
 sys.path.append('..')
-from alpha.platforms.huobi_usdt_swap.rest_market import *
 
-import logging
 logger = logging.getLogger()
 stream_handler = logging.StreamHandler(sys.stdout)
 logger.level = logging.DEBUG
 logger.addHandler(stream_handler)
 
+
 class TestRestMarket(unittest.TestCase):
     def test_get_contract_info(self):
         api = RestMarket()
-        result = api.get_contract_info({"contract_code":"btc-usdt","support_margin_mode":"all"})
-        #logger.info(result)
+        result = api.get_contract_info(
+            {"contract_code": "btc-usdt", "support_margin_mode": "all"})
+        logger.info(result)
         self.assertEqual('ok', result['status'])
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
