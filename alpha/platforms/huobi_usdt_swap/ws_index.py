f5c866c8d78d036993c6ee7627a73608cc3a1aca
adlong <3140618@163.com>
2021-03-02 10:42:51 +0800

    update ws index

--- alpha/platforms/huobi_usdt_swap/ws_index.py
+++ alpha/platforms/huobi_usdt_swap/ws_index.py
@@ -1,84 +1,12 @@
 from alpha.platforms.huobi_usdt_swap.ws_utils import *
 
 
-class WsIndex:
-    def __init__(self, path: str, host: str = None):
-        self.ws = WsUtils(path, host)
+class WsIndex(WsUtils):
+    def __init__(self, host: str = None):
+        super(WsIndex, self).__init__("/ws_index", host)
 
-    def __del__(self):
-        self.ws.close()
+    def sub(self, data:dict, callback):
+        self._sub(json.dumps(data), callback)
 
-    def close(self):
-        self.ws.close()
-
-    def sub_index(self, contract_code: str, period: str, callback):
-        ch = "market.{}.index.{}".format(contract_code, period)
-        id = "sub_index_{}_{}".format(contract_code, period)
-
-        req = {"sub": ch, "id": id}
-        self.ws.sub(req, id, callback)
-
-    def req_index(self, contract_code: str, period: str, start: int, to: int, callback):
-        ch = "market.{}.index.{}".format(contract_code, period)
-        id = "req_index_{}_{}_{}_{}".format(contract_code, period, start, to)
-
-        req = {"req": ch, "from": start, "to": to, "id": id}
-        self.ws.req(req, id, callback)
-
-    def sub_premium_index(self, contract_code: str, period: str, callback):
-        ch = "market.{}.premium_index.{}".format(contract_code, period)
-        id = "sub_premium_index_{}_{}".format(contract_code, period)
-
-        req = {"sub": ch, "id": id}
-        self.ws.sub(req, id, callback)
-
-    def req_premium_index(self, contract_code: str, period: str, start: int, to: int, callback):
-        ch = "market.{}.premium_index.{}".format(contract_code, period)
-        id = "req_premium_index_{}_{}_{}_{}".format(contract_code, period, start, to)
-
-        req = {"req": ch, "from": start, "to": to, "id": id}
-        self.ws.req(req, id, callback)
-
-    def sub_estimated_rate(self, contract_code: str, period: str, callback):
-        ch = "market.{}.estimated_rate.{}".format(contract_code, period)
-        id = "sub_estimated_rate_{}_{}".format(contract_code, period)
-
-        req = {"sub": ch, "id": id}
-        self.ws.sub(req, id, callback)
-
-    def req_estimated_rate(self, contract_code: str, period: str, start: int, to: int, callback):
-        ch = "market.{}.estimated_rate.{}".format(contract_code, period)
-        id = "req_estimated_rate_{}_{}_{}_{}".format(contract_code, period, start, to)
-
-        req = {"req": ch, "from": start, "to": to, "id": id}
-        self.ws.req(req, id, callback)
-
-
-    def sub_basis(self, contract_code: str, period: str, callback):
-        ch = "market.{}.basis.{}.open".format(contract_code, period)
-        id = "sub_basis_{}_{}".format(contract_code, period)
-
-        req = {"sub": ch, "id": id}
-        self.ws.sub(req, id, callback)
-
-    def req_basis(self, contract_code: str, period: str, start: int, to: int, callback):
-        ch = "market.{}.basis.{}.open".format(contract_code, period)
-        id = "req_basis_{}_{}_{}_{}".format(contract_code, period, start, to)
-
-        req = {"req": ch, "from": start, "to": to, "id": id}
-        self.ws.req(req, id, callback)
-
-
-    def sub_mark_price(self, contract_code: str, period: str, callback):
-        ch = "market.{}.mark_price.{}".format(contract_code, period)
-        id = "sub_mark_price_{}_{}".format(contract_code, period)
-
-        req = {"sub": ch, "id": id}
-        self.ws.sub(req, id, callback)
-
-    def req_mark_price(self, contract_code: str, period: str, start: int, to: int, callback):
-        ch = "market.{}.mark_price.{}".format(contract_code, period)
-        id = "req_mark_price_{}_{}_{}_{}".format(contract_code, period, start, to)
-
-        req = {"req": ch, "from": start, "to": to, "id": id}
-        self.ws.req(req, id, callback)
+    def req(self, data:dict, callback):
+        self._req(json.dumps(data), callback)
--- tests/test_ws_index.py
+++ tests/test_ws_index.py
@@ -9,86 +9,39 @@ from alpha.platforms.huobi_usdt_swap.ws_index import *
 
 
 class TestWsIndex(unittest.TestCase):
-    
-    @classmethod
-    def setUpClass(cls):
-        cls.ws = WsIndex("/ws_index")
-
     def _callback_1(self, jdata):
         logger.info('_callback_1:{}'.format(jdata))
 
     def _callback_2(self, jdata):
         logger.info('_callback_2:{}'.format(jdata))
 
-    def test_sub_index(self):
-        self.ws.sub_index("btc-usdt", "1min", self._callback_1)
-        self.ws.sub_index("eth-usdt", "1min", self._callback_2)
+    def test_sub(self):
+        ws1 = WsIndex()
+        data = {"sub": "market.BTC-USDT.index.1min"}
+        ws1.sub(data, self._callback_1)
 
-        time.sleep(10)
-        self.ws.close()
+        ws2 = WsIndex()
+        data = {"sub": "market.ETH-USDT.basis.1min.open"}
+        ws2.sub(data, self._callback_2)
 
-    def test_req_index(self):
-        self.ws.req_index("btc-usdt", "1min", 1614320780, 1614321780, self._callback_1)
-        self.ws.req_index("btc-usdt", "1min", 1614321780, 1614321780, self._callback_2)
+        time.sleep(60)
+        ws1.close()
+        ws2.close()
 
-        time.sleep(10)
-        self.ws.close()
+    def test_req(self):
+        ws1 = WsIndex()
+        data = {"req": "market.BTC-USDT.index.1min",
+                "from": 1614320780, "to": 1614321780}
+        ws1.req(data, self._callback_1)
 
-    def test_sub_premium_index(self):
-        self.ws.sub_premium_index("btc-usdt", "1min", self._callback_1)
-        self.ws.sub_premium_index("eth-usdt", "1min", self._callback_2)
+        ws2 = WsIndex()
+        data = {"req": "market.ETH-USDT.basis.1min.open",
+                "from": 1614321780, "to": 1614321780}
+        ws2.req(data, self._callback_2)
 
-        time.sleep(10)
-        self.ws.close()
-
-    def test_req_premium_index(self):
-        self.ws.req_premium_index("btc-usdt", "1min", 1614320780, 1614321780, self._callback_1)
-        self.ws.req_premium_index("btc-usdt", "1min", 1614321780, 1614321780, self._callback_2)
-
-        time.sleep(10)
-        self.ws.close()
-
-    def test_sub_estimated_rate(self):
-        self.ws.sub_estimated_rate("btc-usdt", "1min", self._callback_1)
-        self.ws.sub_estimated_rate("eth-usdt", "1min", self._callback_2)
-
-        time.sleep(10)
-        self.ws.close()
-
-    def test_req_estimated_rate(self):
-        self.ws.req_estimated_rate("btc-usdt", "1min", 1614320780, 1614321780, self._callback_1)
-        self.ws.req_estimated_rate("btc-usdt", "1min", 1614321780, 1614321780, self._callback_2)
-
-        time.sleep(10)
-        self.ws.close()
-
-    def test_sub_basis(self):
-        self.ws.sub_basis("btc-usdt", "1min", self._callback_1)
-        self.ws.sub_basis("eth-usdt", "1min", self._callback_2)
-
-        time.sleep(10)
-        self.ws.close()
-
-    def test_req_basis(self):
-        self.ws.req_basis("btc-usdt", "1min", 1614320780, 1614321780, self._callback_1)
-        self.ws.req_basis("btc-usdt", "1min", 1614321780, 1614321780, self._callback_2)
-
-        time.sleep(10)
-        self.ws.close()
-
-    def test_sub_mark_price(self):
-        self.ws.sub_mark_price("btc-usdt", "1min", self._callback_1)
-        self.ws.sub_mark_price("eth-usdt", "1min", self._callback_2)
-
-        time.sleep(10)
-        self.ws.close()
-
-    def test_req_mark_price(self):
-        self.ws.req_mark_price("btc-usdt", "1min", 1614320780, 1614321780, self._callback_1)
-        self.ws.req_mark_price("btc-usdt", "1min", 1614321780, 1614321780, self._callback_2)
-
-        time.sleep(10)
-        self.ws.close()
+        time.sleep(60)
+        ws1.close()
+        ws2.close()
 
 
 if __name__ == '__main__':
